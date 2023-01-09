e78c92fa175152b64cdd8e4d4c791b324284d8d8
RobinJunod <82818451+RobinJunod@users.noreply.github.com>
2023-01-09 16:28:29 +0100

    adding figure end last part of the task 6

--- Deliverable_6_1/Deliverable_6_1.m
+++ Deliverable_6_1/Deliverable_6_1.m
@@ -5,10 +5,12 @@ clear all ;close all ;clc
 Ts = 1/20;
 rocket = Rocket(Ts);
 
+% NMPC
 H = 2; % Horizon length in second
 nmpc = NmpcControl(rocket, H);
 
-%% Evaluate once and plot optimal open−loop trajectory
+
+%% Evaluate once and plot optimal open−loop trajectory NMPC
 % pad last input to get consistent size with time and state
 % (w, phi, v, p) Initial state
 x0 = [deg2rad([0 0 0, 0 0 0]), 0 0 0, 0 0 0]';
@@ -18,10 +20,11 @@ ref = [0 0 1 0]';
 U_opt(:,end+1) = nan;
 ph = rocket.plotvis(T_opt, X_opt, U_opt, ref);
 
-%% plot in closed loop with max roll = 15
-Tf = 30;
+
+%% plot in closed loop with max roll = 15 NMPC
 x0 = [deg2rad([0 0 0, 0 0 0]), 0 0 0, 0 0 0]';
 ref = @(t_, x_) ref_EPFL(t_);
+Tf = 30;
 [T, X, U, Ref] = rocket.simulate(x0, Tf, @nmpc.get_u, ref);
 
 % Plot pose
@@ -30,10 +33,9 @@ ph = rocket.plotvis(T, X, U, Ref);
 ph.fig.Name = 'NMPC in nonlinear simulation (default {\gamma}_{ref}=15 deg)'; % Set a figure title
 
 
-%% With max roll = 50
-
-% MPC reference with specified maximum roll = 50 deg
-roll_max = deg2rad(50);
+%% With max roll = 50 NMPC
+x0 = [deg2rad([0 0 0, 0 0 0]), 0 0 0, 0 0 0]';
+roll_max = deg2rad(50); % MPC reference with specified maximum roll = 50 deg
 ref = @(t_, x_) ref_EPFL(t_, roll_max);
 Tf = 30;
 [T, X, U, Ref] = rocket.simulate(x0, Tf, @nmpc.get_u, ref);
@@ -44,10 +46,49 @@ ph = rocket.plotvis(T, X, U, Ref);
 ph.fig.Name = 'NMPC in nonlinear simulation (default {\gamma}_{ref}=50 deg)'; % Set a figure title
 
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Evaluate once and plot optimal open−loop trajectory,
-% pad last input to get consistent size with time and state
-%[u, T_opt, X_opt, U_opt] = nmpc.get_u(x, ref);
-U_opt(:,end+1) = nan;
-ph = rocket.plotvis(T_opt, X_opt, U_opt, ref);
-Tf = 30;
-[T, X, U, Ref] = rocket.simulate(x0, Tf, @nmpc.get_u, ref);
+%% TODO: This file should produce all the plots for the deliverable
+clear
+close all
+clc
+% Prepare system
+Ts = 1/20; % [s] Sample time
+Tf = 30; % [s] Simulation time
+rocket = Rocket(Ts);
+% get steady state values
+[xs, us] = rocket.trim();
+% linearize
+sys = rocket.linearize(xs, us);
+% create sub system
+[sys_x, sys_y, sys_z, sys_roll] = rocket.decompose(sys, xs, us);
+H = 3; % Horizon length in seconds
+% Define the four MPC controllers
+mpc_x = MpcControl_x(sys_x, Ts, H);
+mpc_y = MpcControl_y(sys_y, Ts, H);
+mpc_z = MpcControl_z(sys_z, Ts, H);
+mpc_roll = MpcControl_roll(sys_roll, Ts, H);
+% Merge four subsystems
+mpc = rocket.merge_lin_controllers(xs, us, mpc_x, mpc_y, mpc_z, mpc_roll);
+
+rocket.mass =  1.8; % Manipulate mass for simulation
+
+%% Ref function for 15 deg
+ref = @(t_ , x_) ref_EPFL(t_);
+x0 = zeros(size(sys.A,1),1);
+% Offset-Free Tracking
+[T, X, U, Ref, Z_hat] = rocket.simulate_est_z(x0, Tf, @mpc.get_u, ref, mpc_z, sys_z);
+% Plot pose
+rocket.anim_rate = 5; % Increase this to make the animation faster
+ph = rocket.plotvis(T, X, U, Ref);
+ph.fig.Name = 'Continuous-time nonlinear model with estimator in closed-loop'; % Set a figure title
+
+
+%% Ref function for 50 deg
+roll_max = deg2rad(50); % MPC reference with specified maximum roll = 50 deg
+ref = @(t_, x_) ref_EPFL(t_, roll_max);
+x0 = zeros(size(sys.A,1),1);
+% Offset-Free Tracking
+[T, X, U, Ref, Z_hat] = rocket.simulate_est_z(x0, Tf, @mpc.get_u, ref, mpc_z, sys_z);
+% Plot pose
+rocket.anim_rate = 5; % Increase this to make the animation faster
+ph = rocket.plotvis(T, X, U, Ref);
+ph.fig.Name = 'Continuous-time nonlinear model with estimator in closed-loop'; % Set a figure title
---
+++ Deliverable_6_1/MpcControl_x.m
@@ -0,0 +1,131 @@
+classdef MpcControl_x < MpcControlBase
+    
+    methods
+        % Design a YALMIP optimizer object that takes a steady-state state
+        % and input (xs, us) and returns a control input
+        function ctrl_opti = setup_controller(mpc, Ts, H)
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % INPUTS
+            %   X(:,1)       - initial state (estimate)
+            %   x_ref, u_ref - reference state/input
+            % OUTPUTS
+            %   U(:,1)       - input to apply to the system
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            N_segs = ceil(H/Ts); % Horizon steps
+            N = N_segs + 1;      % Last index in 1-based Matlab indexing
+
+            [nx, nu] = size(mpc.B);
+            
+            % Targets (Ignore this before Todo 3.2)
+            x_ref = sdpvar(nx, 1);
+            u_ref = sdpvar(nu, 1);
+            
+            % Predicted state and input trajectories
+            X = sdpvar(nx, N);
+            U = sdpvar(nu, N-1);
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            
+            % NOTE: The matrices mpc.A, mpc.B, mpc.C and mpc.D are
+            %       the DISCRETE-TIME MODEL of your system
+            
+            % SET THE PROBLEM CONSTRAINTS con AND THE OBJECTIVE obj HERE
+      
+            
+            %% Constraints sub_sys X with 
+            % x in X = { x | Fx <= f } with x of dim 4
+            F = [0 1 0 0;
+                 0 -1 0 0]; 
+            f = [0.1222; 0.1222];
+            % u in U = { u | Mu <= m } only for the d2
+            M = [1;-1]; m = [0.26; 0.26];
+            % model matricies
+            A = mpc.A;
+            B = mpc.B;
+
+
+            %% TO TUNE (no final set)
+            % cost matrices depending on the inupt and state
+%             Q = 500 * eye(size(mpc.A,2));
+%             R = 0.8;
+            Q = [250 0 0 0;
+                 0 1 0 0;
+                 0 0 1 0;
+                 0 0 0 150];
+            R = 10;
+            
+            % WITH YALIMP mpc problem
+            con = (X(:,2) == A*X(:,1) + B*U(:,1)) + (M*U(:,1) <= m);
+            obj = (U(:,1)-u_ref)'*R*(U(:,1)-u_ref);
+            for i = 2:N-1
+                con = con + (X(:,i+1) == A*X(:,i) + B*U(:,i));
+                con = con + (F*X(:,i) <= f) + (M*U(:,i) <= m);
+                % objective (what to minimize -> diff btwn ref and state)
+                u_ = U(:,i)-u_ref;
+                x_ = X(:,i)-x_ref;
+                obj = obj + x_'*Q*x_ + u_'*R*u_;
+            end
+
+
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            % Return YALMIP optimizer object
+            ctrl_opti = optimizer(con, obj, sdpsettings('solver','gurobi'), ...
+                {X(:,1), x_ref, u_ref}, {U(:,1), X, U});
+        end
+        
+        % Design a YALMIP optimizer object that takes a position reference
+        % and returns a feasible steady-state state and input (xs, us)
+        function target_opti = setup_steady_state_target(mpc)
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % INPUTS
+            %   ref    - reference to track
+            % OUTPUTS
+            %   xs, us - steady-state target
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            nx = size(mpc.A, 1);
+
+            % Steady-state targets
+            xs = sdpvar(nx, 1);
+            us = sdpvar;
+            
+            % Reference position (Ignore this before Todo 3.2)
+            ref = sdpvar;
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            % You can use the matrices mpc.A, mpc.B, mpc.C and mpc.D
+            
+            % model matricies
+            A = mpc.A; B = mpc.B;
+            C = mpc.C; D = mpc.D;
+            % Constraints
+            % u in U = { u | Mu <= m } : 
+            M = [1; -1] ; m = [0.26; 0.26];
+            % x in X = { x | Fx <= f }
+            % F*d_x <= 0.1222 - 0
+            F = [0 1 0 0;
+                0 -1 0 0]; 
+            f = [0.1222;0.1222];
+            
+            
+            con = [(xs == A*xs + B*us),...
+                   (M*us <= m)   ,...
+                   (F*xs <= f)   ,...
+                   ref == C*xs + D];
+            obj = us'*us;
+
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            % Compute the steady-state target
+            target_opti = optimizer(con, obj, sdpsettings('solver', 'gurobi'), ref, {xs, us});
+        end
+    end
+end
